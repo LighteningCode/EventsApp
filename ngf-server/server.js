624556a43645f3e6cec1050106f9e1fa9f7fa128
LigtheningCode <elvisagbesi@gmail.com>
2020-03-30 11:25:07 +0000

    fix for event app

---
+++ ngf-server/server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var  port = process.env.PORT || 8808
+
+var app = express();
+
+
+app.use(express.static(__dirname +'../../dist/ng-fundamentals'));
+// Start the app by listening on the default
+
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+    res.sendFile(path.resolve(__dirname + '../../dist/ng-fundamentals'));
+});
+
+
+require('./expressConfig')(app);
+
+require('./passport')();
+
+require('./routes')(app);
+
+
+
+
+app.listen(port);
+console.log('Listening on port ' + port + '...');
